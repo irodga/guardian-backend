5dc302005fec361eb92b6d8afd45593dd72c8da0
irodga <ivanrdz@gmail.com>
2025-04-20 19:04:26 -0600

    feat: backend funcional base con ping a /Vault/ping

--- src/VaultAPI/Controllers/HealthController.cs
+++ src/VaultAPI/Controllers/HealthController.cs
@@ -7,3 +7,12 @@ public class HealthController : ControllerBase
     [HttpGet]
     public IActionResult Get() => Ok("Guardian backend is up!");
 }
+
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok("Guardian backend is up!");
+}
