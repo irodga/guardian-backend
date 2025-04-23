c781758f19bdb906d149a13a917b0545d8e8ed5d
irodga <ivanrdz@gmail.com>
2025-04-23 12:25:49 -0600

    test

--- src/VaultAPI/Models/LoginModel.cs
+++ src/VaultAPI/Models/LoginModel.cs
@@ -6,10 +6,10 @@ namespace VaultAPI.Models
     public class LoginModel
     {
         [Required]
-        public string Username { get; set; }
+        public string Username { get; set; } = string.Empty;
 
         [Required]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
     }
 }
--- src/VaultAPI/Models/ViewModels/DashboardViewModel.cs
+++ src/VaultAPI/Models/ViewModels/DashboardViewModel.cs
@@ -2,10 +2,10 @@
 namespace VaultAPI.Models.ViewModels
 {
     public class DashboardViewModel
-    {
-        public int SecretsCount { get; set; }
-        public int AccessCount { get; set; }
-        public List<Secret> RecentSecrets { get; set; }
-        public List<SecretAuditLog> RecentAccesses { get; set; }
-    }
+{
+    public int SecretsCount { get; set; }
+    public int AccessCount { get; set; }
+    public List<Secret> RecentSecrets { get; set; } = new List<Secret>();
+    public List<SecretAccess> RecentAccesses { get; set; } = new List<SecretAccess>();
+}
 }
