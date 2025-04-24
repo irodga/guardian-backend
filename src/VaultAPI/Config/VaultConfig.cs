a7be8fb2f91c89b4be4e111bd78d25dc892633ab
irodga <ivanrdz@gmail.com>
2025-04-24 11:40:55 -0600

    test

--- src/VaultAPI/Config/VaultConfig.cs
+++ src/VaultAPI/Config/VaultConfig.cs
@@ -1,5 +1,10 @@
-// Archivo: VaultAPI/Config/VaultConfig.cspublic class VaultConfig
+// Ruta: src/VaultAPI/Config/VaultConfig.cs
+
+namespace VaultAPI
 {
-    public string VaultAddress { get; set; } = string.Empty;
-    public string VaultToken { get; set; } = string.Empty;  // Si decides almacenar el token aquí
+    public class VaultConfig
+    {
+        public string VaultAddress { get; set; } = string.Empty;  // Dirección de Vault
+        public string VaultToken { get; set; } = string.Empty;    // Si alguna vez decides almacenar el token aquí
+    }
 }
