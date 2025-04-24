8ab8aa6449de540fe98b9cff19e7e1d0909dd0cc
irodga <ivanrdz@gmail.com>
2025-04-24 15:59:58 -0600

    test

--- src/VaultAPI/Models/CreateCompanyDto.cs
+++
@@ -1,9 +0,0 @@
-// src/VaultAPI/Models/CreateCompanyDto.cs
-namespace VaultAPI.Models
-{
-    public class CreateCompanyDto
-    {
-        public string Name { get; set; } = string.Empty;
-        public int GroupId { get; set; }
-    }
-}
