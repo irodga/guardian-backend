a17d302a5ee1d8ecc40f6af25559c07bbf9004bc
irodga <ivanrdz@gmail.com>
2025-04-24 19:24:22 -0600

    test

--- src/VaultAPI/Controllers/CreateSecretViewModel.cs
+++ src/VaultAPI/Controllers/CreateSecretViewModel.cs
@@ -3,10 +3,7 @@ namespace VaultAPI.Models.Dto
 {
     public class CreateSecretViewModel
     {
-        // Lista de empresas para asociar a un secreto
-        public List<Company> Companies { get; set; }
-
-        // Lista de grupos de empresas para asociar a un secreto
-        public List<Group> Groups { get; set; }  // Usamos 'Group' en lugar de 'CompanyGroup'
+        // Sin datos
+        
     }
 }
